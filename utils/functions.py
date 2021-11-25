052b07b8fbc5b1815bfcd91152a32223725f149a
Inyoung Paik <30650335+humandream1@users.noreply.github.com>
2021-11-25 15:11:31 +0900

    Add files via upload

--- utils/functions.py
+++ utils/functions.py
@@ -1,8 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import torch 
 
 def convert_to_label(pred, confident=0.5):
     return (pred >confident) * 1
+
+def get_IoU(pred, target, confident=0.5) : 
+    B,C,H,W = pred.shape 
+    pred = (pred.cpu()>confident).numpy() # boolean array (B,C,H,W)
+    pred_background = np.logical_not(np.any(pred, axis=1, keepdims=True)) # (B,1,H,W)
+    pred = np.concatenate([pred_background, pred], axis=1) # (B,C+1,H,W)
+    
+    target = (target.cpu()>0.5).numpy()
+    target_background = np.logical_not(np.any(target, axis=1, keepdims=True))
+    target = np.concatenate([target_background, target], axis=1)
+    
+    
+    intersection = np.logical_and(pred, target) # (B,C+1,H,W)
+    union = np.logical_or(pred, target)
+    
+    intersection = intersection.astype(int).sum(axis=(0,2,3))
+    union = union.astype(int).sum(axis=(0,2,3))
+    IoU = intersection/np.clip(union, a_min=1, a_max=None)
+    mIoU = np.mean(IoU)
+    
+    return intersection, union, IoU, mIoU
 
 
 def convert_to_RGB_image(img):
@@ -12,7 +34,8 @@ def convert_to_RGB_image(img):
     RGB_img = np.zeros([Nx, Ny, 3], dtype=np.int)
 
     for c in range(3):
-        RGB_img[:, :, c] = (img[c, :, :] == 1).astype(np.uint8) * 128
+        #RGB_img[:, :, c] = (img[c, :, :] == 1).astype(np.uint8) * 128
+        RGB_img[:, :, c] = (img[c, :, :]*128).astype(np.uint8)
 
     return RGB_img
 
@@ -28,3 +51,14 @@ def save_image(img, path):
     plt.axis('off')
     fig.savefig(path)
     plt.close(fig)
+    
+    
+
+class weighted_BCE(torch.nn.Module) : 
+    def __init__(self, weight=10) : 
+        super().__init__()
+        self.weight = weight 
+    def forward(self, pred, target) : 
+        BCE = torch.nn.BCELoss(weight=target*(self.weight-1)+1)
+        return BCE(pred, target)
+        
--- utils/parse_args.py
+++ utils/parse_args.py
@@ -3,18 +3,20 @@ import argparse
 def parse_args():
     parser = argparse.ArgumentParser()
 
-    parser.add_argument("--model", default='SETR_Naive_S', help='SETR_Naive_S, SETR_Naive_L, SETR_Naive_H, SETR_PUP_S, '
+    parser.add_argument("--model", default='UNet', help='SETR_Naive_S, SETR_Naive_L, SETR_Naive_H, SETR_PUP_S, '
                                                   ' SETR_PUP_L, SETR_PUP_H, SETR_MLA_S, SETR_MLA_L, SETR_MLA_H')
     # ckpt
     parser.add_argument("--ckpt_path", default='.\\ckpt',type=str, help='save directory for check point')
     parser.add_argument("--data_path", default='.\\oct_data', type=str, help='path for data')
 
     # hyper-parameter
-    parser.add_argument("--batch_size", default=2, type=int)
-    parser.add_argument("--epochs", default=50, type=int)
+    parser.add_argument("--batch_size", default=4, type=int)
+    parser.add_argument("--epochs", default=10, type=int)
     parser.add_argument("--ckpt", default=50, type=int)
-    parser.add_argument("--lr", default=1e-4, type=float)
+    parser.add_argument("--lr", default=1e-3, type=float)
     parser.add_argument("--crop_size", default=(480, 480), type=tuple)
+    parser.add_argument("--loss_weight", default=10, type=float)
+    parser.add_argument("--fast_pass", default=False, action='store_true')
 
 
     return parser.parse_args()
